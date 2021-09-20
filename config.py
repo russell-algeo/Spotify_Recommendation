52e8d83f5329b44381ad1973b3beefcd5b28fefa
russell-algeo <77716916+russell-algeo@users.noreply.github.com>
2021-09-20 14:19:50 -0700

    finalize config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+"""
+
+Created on Mon May 23 2021
+
+@author: russellalgeo
+
+"""
+
+#insert local parameters
+
+username = #######
+client_id = #########
+client_secret = ##########
+redirect_uri = ##########
+scope = 'user-library-read playlist-modify-public playlist-read-private user-read-recently-played'
